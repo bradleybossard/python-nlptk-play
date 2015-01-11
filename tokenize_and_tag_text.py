0c51c0112dadbdc0bb6e8ce41d866692661e52c3
Bradley Bossard <bradleybossard@gmail.com>
2015-01-11 10:07:28 -0800

    Add example for tokenizing a sentence and tagging parts of speech.

---
+++ tokenize_and_tag_text.py
@@ -0,0 +1,15 @@
+"""
+Tag sentences with parts of speech.
+"""
+
+import nltk
+
+sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
+
+tokens = nltk.word_tokenize(sentence)
+
+#print tokens
+
+tagged = nltk.pos_tag(tokens)
+
+print tagged
