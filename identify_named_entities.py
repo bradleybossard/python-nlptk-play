787bdf10259b3e4f830cd280f0ddaee47a93ff05
Bradley Bossard <bradleybossard@gmail.com>
2015-01-11 10:07:51 -0800

    Added example for recoginizing a named entitity.

---
+++ identify_named_entities.py
@@ -0,0 +1,14 @@
+"""
+Identify a named entity in the text.
+"""
+
+import nltk
+
+sentence = "At eight o'clock on Thursday morning Arthur didn't feel very good."
+
+tokens = nltk.word_tokenize(sentence)
+tagged = nltk.pos_tag(tokens)
+entities = nltk.chunk.ne_chunk(tagged)
+
+print entities
+
